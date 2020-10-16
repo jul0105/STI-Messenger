9686b8d8ebfd5dafb02ddfa313263a287ec02328
Julien Béguin <julien.beguin99@gmail.com>
2020-10-16 16:27:53 +0200

    add functions comments

--- functions.php
+++ functions.php
@@ -5,11 +5,19 @@
  * Date : 16.10.2020
  */
 
+/**
+ * Set a new flash message (info message at the top of the next page) in session
+ * @param string $content Flash content
+ * @param string $type Type of flash (color)
+ */
 function setFlash($content, $type = 'success') {
     $_SESSION['flash']['content'] = $content;
     $_SESSION['flash']['type'] = $type;
 }
 
+/**
+ * Get flash message in session
+ */
 function getFlash() {
     if (isset($_SESSION['flash'])) {
         echo '<div class="alert alert-' . $_SESSION['flash']['type'] . ' alert-dismissible fade show mb-4">
@@ -22,6 +30,10 @@ function getFlash() {
     }
 }
 
+/**
+ * Redirect to another page
+ * @param string $page page to redirect to
+ */
 function redirect($page) {
     header('Location: /' . $page);
     die();
